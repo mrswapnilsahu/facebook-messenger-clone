3e8112d1d080a86dec1c8082cb6e84ad3405d7c1
Swapnil Sahu <swapnilsahu4@gmail.com>
2020-07-30 00:32:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAiogQqqZKqce-sW1j9n18sIhVUMGoSCl0",
-    authDomain: "fb-messenger-clone-b316e.firebaseapp.com",
-    databaseURL: "https://fb-messenger-clone-b316e.firebaseio.com",
-    projectId: "fb-messenger-clone-b316e",
-    storageBucket: "fb-messenger-clone-b316e.appspot.com",
-    messagingSenderId: "330927580160",
-    appId: "1:330927580160:web:5df734caa2db0aa9c924e5",
-    measurementId: "G-KBLT7VTY09"
+  //Firebase Config Here
 });
 
 const db = firebaseApp.firestore();
